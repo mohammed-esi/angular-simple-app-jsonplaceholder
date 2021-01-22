8d8511b922983d97e3cda0a6ff760434222edc74
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-22 04:37:05 +0100

    add all component in angular.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarHomeComponent } from './components/navbar-home/navbar-home.component';
+import { UsersComponent } from './components/users/users.component';
+import { NavbarUserComponent } from './components/navbar-user/navbar-user.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarHomeComponent,
+    UsersComponent,
+    NavbarUserComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
