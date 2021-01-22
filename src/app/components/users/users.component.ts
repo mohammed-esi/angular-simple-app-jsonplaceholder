0536e67a024d34836faa0a9a5fe7bb294ab47138
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-23 00:27:13 +0100

    get users in users component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User'
+import { UserService } from '../../services/user.service'
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  loading: boolean = true
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      setTimeout(() => {
+        this.loading = false
+        this.users= users
+      }, 2000);
+    })
   }
 
 }
