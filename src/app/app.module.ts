bbc45c7f31c31ff0d935fb766b41925333621374
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-29 16:25:32 +0100

    edit module of app 'add components'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
+
+import { UserService } from './services/user.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,8 +11,10 @@ import { NavbarHomeComponent } from './components/navbar-home/navbar-home.compon
 import { UsersComponent } from './components/users/users.component';
 import { NavbarUserComponent } from './components/navbar-user/navbar-user.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-import { UserService } from './services/user.service'
+import { PostsComponent } from './components/posts/posts.component';
+import { PostFormComponent } from './components/posts/post-form/post-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoadingPageComponent } from './components/loading-page/loading-page.component'
 
 @NgModule({
   declarations: [
@@ -18,11 +23,16 @@ import { UserService } from './services/user.service'
     NavbarHomeComponent,
     UsersComponent,
     NavbarUserComponent,
-    FooterComponent
+    FooterComponent,
+    PostsComponent,
+    PostFormComponent,
+    LoadingPageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
