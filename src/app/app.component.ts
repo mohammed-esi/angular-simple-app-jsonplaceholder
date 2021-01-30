6fadc6585a09e4f0c5ba448d300ef6d2172961f0
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-30 22:22:53 +0100

    add loading page in general component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-app';
+
+  public showOverlay = true;
+  public showContent = true;
+
+  constructor(private router: Router) { 
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event)
+    })
+  }
+
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.showOverlay = true;
+      this.showContent = false;
+    }
+    if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.showOverlay = false;
+          this.showContent = true;
+        }, 2000);
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.showOverlay = false;
+      this.showContent = true;
+    }
+    if (event instanceof NavigationError) {
+      this.showOverlay = false;
+      this.showContent = true;
+    }
+  }
 }
