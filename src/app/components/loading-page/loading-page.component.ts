30f086436030f2deec5b12082cf62d6931cdf296
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-30 22:23:14 +0100

    Loading component

---
+++ src/app/components/loading-page/loading-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-page',
+  templateUrl: './loading-page.component.html',
+  styleUrls: ['./loading-page.component.css']
+})
+export class LoadingPageComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
