1954bfcb6cd0304c41ab4dd8092e15e6a694adaa
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-01-31 20:49:01 +0100

    add route of get user by Id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from '../app/components/home/home.component'
 import { UsersComponent } from '../app/components/users/users.component'
 import { PostsComponent } from '../app/components/posts/posts.component'
+import { UserComponent } from '../app/components/user/user.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'posts', component: PostsComponent}
+  {path: 'posts', component: PostsComponent},
+  {path: 'user/:id', component: UserComponent}
 ]
 
 @NgModule({
